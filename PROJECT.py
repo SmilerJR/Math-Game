0d9da5f547af37875f0fdcc9a32088a93ced51d6
SmilerJR <j.goodchild06@gmail.com>
2019-12-12 11:22:50 +0000

    Add files via upload

---
+++ PROJECT.py
@@ -0,0 +1,178 @@
+import random
+print ("*---------------------------------*")
+print ("|                                 |")
+print ("|   (◔◡◔)  hello and welcome(◔◡◔)  |")
+print ("|    (◔◡◔)  to my  (◔◡◔)           |")
+print ("|     (◔◡◔)  epic python  (◔◡◔)    |")
+print ("|    (◔◡◔) MATH QUIZZ (◔◡◔)        |")
+print ("|            owo                  |")
+print ("*---------------------------------*")
+print("")
+name = input("Please enter your name? ")
+print("")
+year = input("What year are you in? ")
+print("")
+select = int(input("1. Addition  | 2. Subtraction | 3. Multiplication  | 4. Division "))
+if select == 1:
+    questions = int(input("How Many questions do you want"))
+    for q in range(questions):
+        n1 = random.randint(1, 100)
+        n2 = random.randint(1, 100)
+        print("What is " + str(n1) + " + " + str(n2) + "?")
+        computer_answer = n1 + n2
+        anwser = int(input())
+        if anwser == computer_answer:
+            print ("Correct")
+        else:
+            print ("wrong")
+elif select == 2:
+    questions = int(input("How Many questions do you want"))
+    for q in range(questions):
+        n1 = random.randint(50, 100)
+        n2 = random.randint(1,50)
+        print("What is " + str(n1) + " - " + str(n2) + "?")
+        computer_answer = n1 - n2
+        anwser = int(input())
+        if anwser == computer_answer:
+            print ("Correct")
+        else:
+            print ("wrong")
+elif select == 3:
+    questions = int(input("How Many questions do you want"))
+    for q in range(questions):
+        n1 = random.randint(1, 10)
+        n2 = random.randint(1, 10)
+        print("What is " + str(n1) + " * " + str(n2) + "?")
+        computer_answer = n1 * n2
+        anwser = int(input())
+        if anwser == computer_answer:
+            print ("Correct")
+        else:
+            print ("wrong")
+elif select == 4:
+    questions = int(input("How Many questions do you want"))
+    for q in range(questions):
+        n1 = random.randint(50, 100)
+        n2 = random.randint(1,50)
+        print("What is " + str(n1) + " / " + str(n2) + "?")
+        computer_answer = n1 / n2
+        anwser = int(input())
+        if anwser == computer_answer:
+            print ("Correct")
+        else:
+            print ("wrong")
+else:
+    ("ERROR")
+
+print ("restarting...")
+print ("restarting...")
+print ("restarting...")
+print ("restarting...")
+
+print("")
+print ("*---------------------------------*")
+print ("|                                 |")
+print ("|   (◔◡◔)  hello and welcome(◔◡◔)  |")
+print ("|    (◔◡◔)  to my  (◔◡◔)           |")
+print ("|     (◔◡◔)  epic python  (◔◡◔)    |")
+print ("|    (◔◡◔) MATH QUIZZ (◔◡◔)        |")
+print ("|            owo                  |")
+print ("*---------------------------------*")
+print("")
+name = input("Please enter your name? ")
+print("")
+year = input("What year are you in? ")
+print("")
+select = int(input("1. Addition  | 2. Subtraction | 3. Multiplication  | 4. Division "))
+if select == 1:
+    questions = int(input("How Many questions do you want"))
+    for q in range(questions):
+        n1 = random.randint(1, 100)
+        n2 = random.randint(1, 100)
+        print("What is " + str(n1) + " + " + str(n2) + "?")
+        computer_answer = n1 + n2
+        anwser = int(input())
+        if anwser == computer_answer:
+            print ("Correct")
+        else:
+            print ("wrong")
+elif select == 2:
+    questions = int(input("How Many questions do you want"))
+    for q in range(questions):
+        n1 = random.randint(50, 100)
+        n2 = random.randint(1,50)
+        print("What is " + str(n1) + " - " + str(n2) + "?")
+        computer_answer = n1 - n2
+        anwser = int(input())
+        if anwser == computer_answer:
+            print ("Correct")
+        else:
+            print ("wrong")
+elif select == 3:
+    questions = int(input("How Many questions do you want"))
+    for q in range(questions):
+        n1 = random.randint(1, 10)
+        n2 = random.randint(1, 10)
+        print("What is " + str(n1) + " * " + str(n2) + "?")
+        computer_answer = n1 * n2
+        anwser = int(input())
+        if anwser == computer_answer:
+            print ("Correct")
+        else:
+            print ("wrong")
+elif select == 4:
+    questions = int(input("How Many questions do you want"))
+    for q in range(questions):
+        n1 = random.randint(50, 100)
+        n2 = random.randint(1,50)
+        print("What is " + str(n1) + " / " + str(n2) + "?")
+        computer_answer = n1 / n2
+        anwser = int(input())
+        if anwser == computer_answer:
+            print ("Correct")
+        else:
+            print ("wrong")
+else:
+    ("ERROR")
+
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("toast")
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
