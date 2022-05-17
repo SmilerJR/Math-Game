b272e53af955f984ffe5f5eb1d6a50c9f802c17d
SmilerJR <j.goodchild06@gmail.com>
2022-05-17 12:44:41 +0100

    Update remake.py

--- remake.py
+++ remake.py
@@ -1,1 +1,22 @@
+import math
+import time
+import random as rnd
 
+name = input("please enter your name \n")
+age = int(input(f"Hello {name} how old are you \n"))
+select = int(input("1. Addition  | 2. Subtraction | 3. Multiplication  | 4. Division \n"))
+
+if select == 1:
+    questions = int(input("How Many questions do you want \n"))
+    for q in range(questions):
+        n1 = rnd.randint(1, 100)
+        n2 = rnd.randint(1, 100)
+        print("What is " + str(n1) + " + " + str(n2) + "?")
+        computer_answer = n1 + n2
+        anwser = int(input())
+        if anwser == computer_answer:
+            print ("Correct")
+            q+=1
+        else:
+            print ("wrong")
+            q+=1
